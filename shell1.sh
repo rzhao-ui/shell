a148074c915c009c0867477d61b135392a534859
rzhao-ui <1147153137@qq.com>
2019-08-21 01:29:38 -0700

    shell

--- shell1.sh
+++ shell1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-not_blank_length=0
+flag=0
 count_length()
 {
 	not_blank_length=0
@@ -9,25 +9,18 @@ count_length()
 		not_blank_length=$((not_blank_length+length_ch))
 	done
 }
+echo "lower:转换为小写 upper:转换为小写 quit或exit:退出交互程序"
 while true
 do
-	read -p "请输入字符串：" string
-	read -p "请输入命令：" cmd
-	case "$cmd" in
+	read -p "请输入字符串：" string 
+	case $string in
 		"lower")
-			echo $string | tr "[:upper:]" "[:lower:]"
-			count_length
-			echo "非空格数为：$not_blank_length"
+			flag=1
+			echo "进入小写模式"
 			;;
 		"upper")
-			echo $string | tr "[:lower:]" "[:upper:]"
-			count_length
-			echo "非空格数为：$not_blank_length"
-			;;
-		"")
-			echo $string | tr "[:upper:]" "[:lower:]"
-			count_length
-			echo "非空格数为：$not_blank_length"
+			flag=2
+			echo "进入大写模式"
 			;;
 		"exit")
 			exit
@@ -36,7 +29,23 @@ do
 			exit
 			;;
 		*)
-			echo "命令字符串为lower,upper,exit,quit"
-			;;
-	esac
+			case $flag in
+				1)
+			 		echo $string | tr "[:upper:]" "[:lower:]"
+					count_length 
+					echo "非空格字符数为：$not_blank_length"
+					;;
+				2)
+					echo $string | tr "[:lower:]" "[:upper:]"
+					count_length 
+					echo "非空格字符数为：$not_blank_length"
+					;;
+				*)
+	    			echo $string | tr "[:upper:]" "[:lower:]"
+					count_length 
+					echo "非空格字符数为：$not_blank_length"
+					;;
+			esac
+	esac	
 done
+
